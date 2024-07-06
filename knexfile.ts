5df3c453ef9058f1e6816287e5ffc343caadf489
gabrieu <marques.gabriel@hublocal.com.br>
2024-07-06 11:20:20 -0300

    feat: run migrate for the first time

--- knexfile.ts
+++ knexfile.ts
@@ -4,7 +4,7 @@ import path from "path";
 export default {
   client: "sqlite3",
   connection: {
-    filename: path.resolve("src", "database", "database.db"),
+    filename: path.resolve("src", "database", "sqlite", "database.db"),
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
